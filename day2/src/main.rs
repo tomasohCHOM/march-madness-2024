6ce8e93eea9dd83138c75312bfad17ce7ca51c19
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-03-26 14:09:43 -0700

    Finish day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,45 +1,38 @@
 use std::fs;
 
-fn day2(file_contents: String) -> (u32, u32) {
-    let part1 = file_contents.chars().filter(|c| *c == '.').count() as u32;
+fn part1(lines: &Vec<String>) -> u32 {
+    return lines
+        .iter()
+        .map(|line| line.chars().filter(|c| *c == '.').count() as u32)
+        .sum::<u32>();
+}
 
-    let mut part2 = 0;
-    let rows = file_contents.lines().count();
-    let cols = file_contents.lines().nth(0).unwrap().len();
+fn part2(contents: &Vec<String>) -> u32 {
+    let mut output = 0;
+    let rows = contents.len();
+    let cols = contents[0].len();
     let square_size = 15;
 
-    for row in 0..rows - square_size {
-        for col in 0..cols - square_size {
-            let mut flag = true;
-            for i in 0..square_size {
-                for j in 0..square_size {
-                    if file_contents
-                        .lines()
-                        .nth(row + i)
-                        .unwrap()
-                        .chars()
-                        .nth(col + j)
-                        .unwrap()
-                        == '#'
-                    {
-                        flag = false;
-                        break;
-                    }
-                }
-                if !flag {
-                    break;
-                }
-                part2 += 1;
-                println!("{}", part2);
+    for row in 0..rows - square_size + 1 {
+        for col in 0..cols - square_size + 1 {
+            if (0..square_size).all(|i| {
+                (0..square_size).all(|j| contents[row + i].chars().nth(col + j).unwrap() != '#')
+            }) {
+                output += 1;
             }
         }
-        println!("{:?}", file_contents.lines().nth(row).unwrap());
     }
 
-    return (part1, part2);
+    return output;
 }
 
 fn main() {
-    let file_contents = fs::read_to_string("input.txt").expect("Unable to read file");
-    println!("{:?}", day2(file_contents));
+    let lines: Vec<String> = fs::read_to_string("input.txt")
+        .expect("Unable to read file")
+        .lines()
+        .map(String::from)
+        .collect();
+
+    println!("Part 1 Result: {:?}", part1(&lines));
+    println!("Part 2 Result: {:?}", part2(&lines));
 }
