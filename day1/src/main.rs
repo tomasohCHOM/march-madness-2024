eb0f3be584937333fa9c845cf569e1af76702195
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-03-26 11:41:54 -0700

    Modify day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,19 +1,21 @@
 use std::fs;
 
-fn day1(file_contents: String) -> (u32, u32) {
-    let mut part1: u32 = 0;
+fn part1(file_contents: &String) -> u32 {
+    let mut output: u32 = 0;
     for line in file_contents.lines() {
         if line.starts_with("[STOP]") {
-            part1 += 1;
+            output += 1;
         }
     }
+    return output;
+}
 
-    let part2 = part1 + file_contents.lines().count() as u32;
-
-    return (part1, part2);
+fn part2(file_contents: &String) -> u32 {
+    return part1(&file_contents) + file_contents.lines().count() as u32;
 }
 
 fn main() {
     let file_contents = fs::read_to_string("input.txt").expect("Unable to read file");
-    println!("{:?}", day1(file_contents));
+    println!("Part 1 Result: {:?}", part1(&file_contents));
+    println!("Part 2 Result: {:?}", part2(&file_contents));
 }
