b871152e398d79410fbbf195c17a610f7578e9c2
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-03-27 18:12:17 -0700

    Add day3 solution (i think its correct)

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,12 +1,37 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs;
 
-fn part1(ingredients_list: &HashMap<String, Vec<String>>) -> u32 {
-    return 0;
+fn part1(ingredients_list: &HashMap<String, Vec<String>>, base: &String) -> u32 {
+    let mut q: VecDeque<&String> = VecDeque::from([base]);
+    let mut output: HashSet<&String> = HashSet::new();
+    while !q.is_empty() {
+        let ingredient = q.pop_front().unwrap();
+        output.insert(ingredient);
+        if ingredients_list.contains_key(ingredient) {
+            for item in ingredients_list.get(ingredient).unwrap() {
+                q.push_back(item);
+            }
+        }
+    }
+    return output.len() as u32;
 }
 
-fn part2(ingredients_list: &HashMap<String, Vec<String>>) -> u32 {
-    return 0;
+fn part2(ingredients_list: &HashMap<String, Vec<String>>, needed_list: &Vec<String>) -> u32 {
+    let mut output: HashSet<&String> = HashSet::new();
+    for needed in needed_list {
+        let mut q: VecDeque<&String> = VecDeque::from([needed]);
+        while !q.is_empty() {
+            let ingredient = q.pop_front().unwrap();
+            if ingredients_list.contains_key(ingredient) {
+                for item in ingredients_list.get(ingredient).unwrap() {
+                    q.push_back(item);
+                }
+            } else {
+                output.insert(ingredient);
+            }
+        }
+    }
+    return output.len() as u32;
 }
 
 fn main() {
@@ -22,12 +47,21 @@ fn main() {
         let k = list[0].to_string();
         let v = list[1]
             .to_string()
-            .split(" = ")
+            .split(" + ")
             .map(str::to_string)
             .collect();
         ingredients_list.insert(k, v);
     }
 
-    println!("Part 1 Result: {:?}", part1(&ingredients_list));
-    println!("Part 2 Result: {:?}", part2(&ingredients_list));
+    let needed = (&lines[0]).split(": ").nth(1).unwrap().to_string();
+    let needed_list: Vec<String> = needed.split(", ").map(str::to_string).collect();
+
+    println!(
+        "Part 1 Result: {:?}",
+        part1(&ingredients_list, &needed_list[0])
+    );
+    println!(
+        "Part 2 Result: {:?}",
+        part2(&ingredients_list, &needed_list)
+    );
 }
