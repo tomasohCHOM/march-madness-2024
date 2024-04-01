839603ca47b62126cdda3ebd409d71b26e43d6fa
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-04-01 15:22:55 -0700

    Upload day4 part2 solution

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,27 +1,14 @@
-use std::f64::consts::PI;
 use std::fs;
 
 fn area_of_intersection(c1_x: f64, c1_y: f64, r1: f64, c2_x: f64, c2_y: f64, r2: f64) -> f64 {
-    let distance = ((c1_x - c2_x).powi(2) + (c1_y - c2_y).powi(2)).sqrt();
-    if distance > r1 + r2 {
-        return 0.0;
-    } else if distance < f64::abs(r1 - r2) {
-        return PI * f64::min(r1, r2).powi(2);
-    }
-    let area_c1 = PI * r1.powi(2);
-    let area_c2 = PI * r2.powi(2);
-    let a = r1;
-    let b = r2;
-    let c = distance;
-    let angle = (a.powi(2) + c.powi(2) - b.powi(2)) / (2.0 * a * c);
-    let theta = 2.0 * f64::acos(angle);
-
-    let overlap_sector = (theta / (2.0 * PI)) * area_c1;
-
-    let triangle_area =
-        0.5 * distance * f64::sqrt(f64::max(0.0, r1.powi(2) + distance.powi(2) - r2.powi(2)));
-
-    return overlap_sector - triangle_area;
+    let rr1: f64 = r1 * r1;
+    let rr2: f64 = r2 * r2;
+    let c: f64 = ((c2_x - c1_x).powi(2) + (c2_y - c1_y).powi(2)).sqrt();
+    let phi: f64 = ((rr1 + (c * c) - rr2) / (2.0 * r1 * c)).acos() * 2.0;
+    let theta: f64 = ((rr2 + (c * c) - rr1) / (2.0 * r2 * c)).acos() * 2.0;
+    let area1: f64 = 0.5 * theta * rr2 - 0.5 * rr2 * theta.sin();
+    let area2: f64 = 0.5 * phi * rr1 - 0.5 * rr1 * phi.sin();
+    return area1 + area2;
 }
 
 fn part1(captures: &Vec<(i32, i32, i32)>) -> u32 {
@@ -39,25 +26,30 @@ fn part1(captures: &Vec<(i32, i32, i32)>) -> u32 {
     return output;
 }
 
-fn part2(captures: &Vec<(i32, i32, i32)>) -> f64 {
+fn part2(captures: &Vec<(i32, i32, i32)>) -> u32 {
     let mut output = 0.0;
     for i in 0..captures.len() {
         for j in (i + 1)..captures.len() {
-            let area: f64 = area_of_intersection(
-                captures[i].0 as f64,
-                captures[i].1 as f64,
-                captures[i].2 as f64,
-                captures[j].0 as f64,
-                captures[j].1 as f64,
-                captures[j].2 as f64,
-            );
+            let xd = captures[i].0 - captures[j].0;
+            let yd = captures[i].1 - captures[j].1;
+            let dist: f32 = f32::sqrt((xd.pow(2) + yd.pow(2)) as f32);
+            if dist <= (captures[i].2 + captures[j].2) as f32 {
+                let area: f64 = area_of_intersection(
+                    captures[i].0 as f64,
+                    captures[i].1 as f64,
+                    captures[i].2 as f64,
+                    captures[j].0 as f64,
+                    captures[j].1 as f64,
+                    captures[j].2 as f64,
+                );
 
-            if area > output {
-                output = area;
+                if area > output {
+                    output = area;
+                }
             }
         }
     }
-    return output;
+    return output as u32;
 }
 
 fn main() {
@@ -99,6 +91,6 @@ fn main() {
 
         captures.push((x, y, r));
     }
-    println!("{}", part1(&captures));
-    println!("{}", part2(&captures));
+    println!("Part 1 Result: {:?}", part1(&captures));
+    println!("Part 2 Result: {:?}", part2(&captures));
 }
